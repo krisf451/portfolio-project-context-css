9af844d4e7a2e65e77237bd82507c6237cf50ec9
Kristian Fulkerson <kristianf451@gmail.com>
2021-10-14 16:37:41 -0700

    first

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "https://images.fineartamerica.com/images-medium-large-5/random-project-plans-scribble-sandy-tolman.jpg",
+    link: "http://lama.dev",
+  },
+  {
+    id: 2,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoBRYJOZKj3b8_jTCiWitaQ5c0T_cddGMiww&usqp=CAU",
+    link: "http://lama.dev",
+  },
+  {
+    id: 3,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7LtpmliPD5wMatY_AoexIrnrO08TFGi7F6A&usqp=CAU",
+    link: "http://lama.dev",
+  },
+  {
+    id: 4,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzs0JBMIjWvq02-kiUOxAJHZeAyVQHpkkqsw&usqp=CAU",
+    link: "http://lama.dev",
+  },
+  {
+    id: 5,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDwQ1PHV6hz7BrJ63RJBsibmCADNLwqNUEmg&usqp=CAU",
+    link: "http://lama.dev",
+  },
+  {
+    id: 6,
+    img: "https://cdn.ucfbootcamps.com/wp-content/uploads/sites/113/2019/12/Var_1-1024x770.jpg",
+    link: "http://lama.dev",
+  },
+];
